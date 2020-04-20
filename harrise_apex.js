36e7f08bbc8d60251c98ed819678559f4393ffa3
harrise <eddy.harris@gmail.com>
2020-04-20 19:55:22 +0800

    Update harrise_apex.js

--- harrise_apex.js
+++ harrise_apex.js
@@ -1,3 +1,4 @@
+
 // Displays a page-level success message ‘Changes saved!’.
 apex.message.showPageSuccess( "Changes saved!" );
 
@@ -6,3 +7,20 @@ if (this.data.successMessage ) {
 
     apex.message.showPageSuccess(this.data.successMessage.text);
 }
+
+
+
+// Key Release 
+// JavaScript Expression
+--
+this.browserEvent.which === 13
+|| this.browserEvent.which === 9
+
+
+
+// buttons classes  - quick
+
+<span class="t-Icon fa fa-gavel" aria-hidden="true"></span>#JOB_NO#
+
+class="t-Button t-Button--icon t-Button--hot t-Button--primary t-Button--simple t-Button--iconLeft"
+
