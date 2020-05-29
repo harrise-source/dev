a0ed6d676a19c0395a95ae170eed2d839d90f0e0
harrise <eddy.harris@gmail.com>
2020-05-29 08:59:37 +0800

    changes

--- code/eh_apex.js
+++ code/eh_apex.js
@@ -1,3 +1,120 @@
+
+
+// DESC : Apex Native loader
+// **********************************************************************************************
+
+// apex show native loader
+//--
+apex.widget.waitPopup();
+
+// apex remove native loadar
+//--
+$("#apex_wait_overlay").remove();
+$(".u*Processing").remove();
+
+// or 
+//--
+var loader;
+loader = apex.widget.waitPopup();
+loader.remove();
+
+
+// with an ajax call
+//--
+apex.server.process(
+  "Set product name", {}, {
+  dataType: 'text',
+  beforeSend: function () {
+    apex.widget.waitPopup();
+  },
+  success: function (pData) {
+    setTimeout(function () {
+      $s("P6_PRODUCT_NAME", pData);
+      $("#apex_wait_overlay").remove();
+      $(".u-Processing").remove();
+    }, 2000);
+  }
+});
+
+//URL : http://oraclemasterminds.blogspot.com/2018/04/displaying*processing*spinners*in*apex.html
+//URL : https://chrisjansen.me/apex-loading-when-processing-ajax-call/
+
+
+
+// apex - waitPopup until an AJAX callback has finished
+//--
+
+var popup = apex.widget.waitPopup();
+var promise = apex.server.process("SUBMIT_REQUEST"
+  , {
+    pageItems: "#P207_SUBMIT_REQUEST_ID"
+  }
+  , {
+    dataType: 'text'
+    , success: function (pData) {
+      apex.region("p207_myreq_reg").refresh();
+    }
+  });
+  
+promise.always(function () {
+  popup.remove();
+});
+
+
+// Displays a page-level success message ‘Changes saved!’.
+apex.message.showPageSuccess( "Changes saved!" );
+
+// show message from dialog page on close
+if (this.data.successMessage ) {
+
+    apex.message.showPageSuccess(this.data.successMessage.text);
+}
+
+
+
+// Displays an alert ‘Load complete.’, then after the dialog closes executes the ‘afterLoad()’ function.
+apex.message.alert( "Load complete.", function(){
+    afterLoad();
+});
+
+
+
+// Key Down!!!   -- the other Key Events don't fire on tab(9)
+
+// JavaScript Expression
+//
+this.browserEvent.which === 13 //enter or 
+|| this.browserEvent.which === 9 //tab
+
+
+
+
+// button builder
+//
+// https://apex.oracle.com/pls/apex/f?p=42:6100:::NO:::
+
+
+// buttons classes  - quick
+
+// link text 
+<span class="t-Icon fa fa-icon_name" aria-hidden="true"></span>#COLUMN#
+
+
+<span class="t-Icon fa fa-gavel" aria-hidden="true"></span>#JOB_NO#
+
+//link attributes
+class="t-Button t-Button--icon t-Button--hot t-Button--primary t-Button--simple t-Button--iconLeft"
+
+class="t-Button t-Button--icon t-Button--primary t-Button--simple t-Button--iconLeft"
+
+t-Button--noUI
+t-Button--simple
+
+"t-Button--stretch"
+
+
+
+
 /****************************************************************************
   jQuery data 
   http://api.jquery.com/data/
